fb09f88a57bf4fd9f62a1729c454199ceaaa2fdc
akshayyadav99 <akshayyadav2525@gmail.com>
2025-12-20 12:23:58 +0530

    Delete Operation Done by Akshay

--- EnquiryModule/src/main/java/com/bankcorp/enquiry/controller/EnquiryController.java
+++ EnquiryModule/src/main/java/com/bankcorp/enquiry/controller/EnquiryController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,13 @@ public class EnquiryController {
 		
 		Enquiry enquiry = enquiryService.updateCibil(customerId,cibilscore);
         return new ResponseEntity<Enquiry>(enquiry, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteById/{customerId}")
+	public ResponseEntity<String> deleteEnquiryData(@PathVariable ("customerId") int customerId)
+	{
+		enquiryService.deleteEnquiryData(customerId);
+		return new ResponseEntity<String>("Data is Deleted", HttpStatus.OK);
 	}
 
 
