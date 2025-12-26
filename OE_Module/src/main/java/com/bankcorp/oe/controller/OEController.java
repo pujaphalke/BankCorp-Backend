9794cccc1fbe3caa16eb0854e0a922f6f373099c
sgonyal <gonyalshubham7@gmail.com>
2025-12-26 23:50:38 +0530

    cross origin added in each controller class by shubham

--- OE_Module/src/main/java/com/bankcorp/oe/controller/OEController.java
+++ OE_Module/src/main/java/com/bankcorp/oe/controller/OEController.java
@@ -2,9 +2,11 @@ package com.bankcorp.oe.controller;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class OEController {
 
